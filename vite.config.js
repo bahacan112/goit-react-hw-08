5e94e2c06fb7b43bdcc803ed97f7fbc73fd21830
bahacan112 <bzenbil19@gmail.com>
2024-12-10 08:46:45 +0300

    vite config düzenlendi

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // Eğer özel bir alt dizine yüklemiyorsanız / kullanın
+  build: {
+    outDir: "dist", // Çıkış klasörünü ayarlayın
+  },
+  server: {
+    port: 3000, // Local sunucunuz için özel bir port
+  },
+});
